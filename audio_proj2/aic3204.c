08cb20b076533b45535094c68c450331625393cf
Ilya Gutnikov <ilyagutnikov93@gmail.com>
2015-10-18 17:28:16 +0400

    Вынес проигрыщ синусоиды и звука с микрфона в main, осталось добавить комменты и можно приступать к работе над эффектом

--- audio_proj2/aic3204.c
+++ audio_proj2/aic3204.c
@@ -15,7 +15,8 @@
 #include "i2s.h"
 #include "stdio.h"
 
-
+#define Rcv 0x08
+#define Xmit 0x20
 /* ------------------------------------------------------------------------ *
  *                                                                          *
  *  _AIC3204_rget( regnum, regval )                                         *
@@ -53,6 +54,72 @@ Int16 AIC3204_rset( Uint16 regnum, Uint16 regval )
     cmd[1] = regval;                // 8-bit Register Data
 
     return I2C_write( AIC3204_I2C_ADDR, cmd, 2 );
+}
+
+Int16 aic3204_sin( )
+{
+/* ------------------------------------------------------------------------ *
+ *                                                                          *
+ *   Configure AIC3204                                                      *
+ *      CODEC_CLKIN = PLL CLK                                               *
+ *      CODEC_CLKIN = NADC * MADC * AOSR * ADCFS                            *
+ *      PLL_CLK     = PLL_CLKIN * R * J.D / P                               *
+ *      PLL_CLKIN   = BCLK = 1.536MHz                                       *
+ *      ADCFS       = 48KHz                                                 *
+ *                                                                          *
+ * ------------------------------------------------------------------------ */
+    AIC3204_rset(  0, 0x00 );      // Select page 0
+    AIC3204_rset(  1, 0x01 );      // Reset codec
+    AIC3204_rset(  0, 0x01 );      // Select page 1
+    AIC3204_rset(  1, 0x08 );      // Disable crude AVDD generation from DVDD
+    AIC3204_rset(  2, 0x00 );      // Enable Analog Blocks
+    // PLL and Clocks config and Power Up
+    AIC3204_rset(  0, 0x00 );      // Select page 0
+    AIC3204_rset( 27, 0x00 );      // BCLK and WCLK is set as i/p to AIC3204(Slave)
+    AIC3204_rset(  4, 0x07 );      // PLL setting: PLLCLK <- BCLK and CODEC_CLKIN <-PLL CLK
+    AIC3204_rset(  6, 0x20 );      // PLL setting: J = 32
+    AIC3204_rset(  7, 0 );         // PLL setting: HI_BYTE(D)
+    AIC3204_rset(  8, 0 );         // PLL setting: LO_BYTE(D)
+    // For 48 KHz sampling
+    AIC3204_rset(  5, 0x92 );      // PLL setting: Power up PLL, P=1 and R=2
+    AIC3204_rset( 13, 0x00 );      // Hi_Byte(DOSR) for DOSR = 128 decimal or 0x0080 DAC oversamppling
+    AIC3204_rset( 14, 0x80 );      // Lo_Byte(DOSR) for DOSR = 128 decimal or 0x0080
+    AIC3204_rset( 20, 0x80 );      // AOSR for AOSR = 128 decimal or 0x0080 for decimation filters 1 to 6
+    AIC3204_rset( 11, 0x84 );      // Power up NDAC and set NDAC value to 4
+    AIC3204_rset( 12, 0x82 );      // Power up MDAC and set MDAC value to 2
+    AIC3204_rset( 18, 0x84 );      // Power up NADC and set NADC value to 4
+    AIC3204_rset( 19, 0x82 );      // Power up MADC and set MADC value to 2
+    // DAC ROUTING and Power Up
+    AIC3204_rset(  0, 0x01 );      // Select page 1
+    AIC3204_rset( 12, 0x08 );      // LDAC AFIR routed to HPL
+    AIC3204_rset( 13, 0x08 );      // RDAC AFIR routed to HPR
+    AIC3204_rset(  0, 0x00 );      // Select page 0
+    AIC3204_rset( 64, 0x02 );      // Left vol=right vol
+    AIC3204_rset( 65, 0x00 );      // Left DAC gain to 0dB VOL; Right tracks Left
+    AIC3204_rset( 63, 0xd4 );      // Power up left,right data paths and set channel
+    AIC3204_rset(  0, 0x01 );      // Select page 1
+    AIC3204_rset( 16, 0x00 );      // Unmute HPL , 0dB gain
+    AIC3204_rset( 17, 0x00 );      // Unmute HPR , 0dB gain
+    AIC3204_rset(  9, 0x30 );      // Power up HPL,HPR
+    AIC3204_rset(  0, 0x00 );      // Select page 0
+    EVM5515_wait( 500 );           // Wait
+    // ADC ROUTING and Power Up
+    AIC3204_rset(  0, 0x01 );      // Select page 1
+    AIC3204_rset( 52, 0x0C );      // STEREO 1 Jack
+		                           // IN2_L to LADC_P through 40 kohm
+    AIC3204_rset( 55, 0x0C );      // IN2_R to RADC_P through 40 kohmm
+    AIC3204_rset( 54, 0x03 );      // CM_1 (common mode) to LADC_M through 40 kohm
+    AIC3204_rset( 57, 0xC0 );      // CM_1 (common mode) to RADC_M through 40 kohm
+    AIC3204_rset( 59, 0x00 );      // MIC_PGA_L unmute
+    AIC3204_rset( 60, 0x00 );      // MIC_PGA_R unmute
+    AIC3204_rset(  0, 0x00 );      // Select page 0
+    AIC3204_rset( 81, 0xc0 );      // Powerup Left and Right ADC
+    AIC3204_rset( 82, 0x00 );      // Unmute Left and Right ADC
+
+    AIC3204_rset( 0,  0x00 );
+    EVM5515_wait( 200 );  // Wait
+
+    return 0;
 }
 
 Int16 aic3204_mic( )
@@ -124,4 +191,14 @@ Int16 aic3204_mic( )
 
     return 0;
 }
+/*
+Int16 aic3204_read(Int16 left, Int16 right) {
 
+
+	while ((Rcv & I2S2_IR) == 0); // Wait for receive interrupt to be pending
+	left = I2S2_W0_MSW_R; // 16 bit left channel received audio data
+	right = I2S2_W1_MSW_R;// 16 bit right channel received audio data
+
+	return 0;
+}
+*/
--- audio_proj2/main.c
+++ audio_proj2/main.c
@@ -10,13 +10,25 @@
 #include "i2s.h"
 #include "stdio.h"
 
+    /* Pre-generated sine wave data, 16-bit signed samples */
+    Int16 sinetable[48] = {
+        0x0000, 0x10b4, 0x2120, 0x30fb, 0x3fff, 0x4dea, 0x5a81, 0x658b,
+        0x6ed8, 0x763f, 0x7ba1, 0x7ee5, 0x7ffd, 0x7ee5, 0x7ba1, 0x76ef,
+        0x6ed8, 0x658b, 0x5a81, 0x4dea, 0x3fff, 0x30fb, 0x2120, 0x10b4,
+        0x0000, 0xef4c, 0xdee0, 0xcf06, 0xc002, 0xb216, 0xa57f, 0x9a75,
+        0x9128, 0x89c1, 0x845f, 0x811b, 0x8002, 0x811b, 0x845f, 0x89c1,
+        0x9128, 0x9a76, 0xa57f, 0xb216, 0xc002, 0xcf06, 0xdee0, 0xef4c
+    };
+
 Int16 j, i = 0;
-Int16 sample, data1, data2, data3, data4;
+Int16 sample, left, right;
 
 #define Rcv 0x08
 #define Xmit 0x20
 
 extern Int16 aic3204_mic();
+extern Int16 aic3204_sin();
+extern Int16 aic3204_read();
 
 void main(void) {
 	/* Initialize BSL */
@@ -33,13 +45,31 @@ void main(void) {
 	EVM5515_GPIO_setOutput(GPIO10, 1);    // Take AIC3201 chip out of reset
 	I2C_init();                    // Initialize I2C
 
+    /* I2S settings */
+    I2S2_SRGR = 0x0015;
+    I2S2_ICMR = 0x0028;    // Enable interrupts
+    I2S2_CR   = 0x8012;    // 16-bit word, Master, enable I2C
+
+    aic3204_sin();
+
+    /* Play Tone */
+    for ( i = 0 ; i < 5 ; i++ )
+    {
+        for ( j = 0 ; j < 1000 ; j++ )
+        {
+            for ( sample = 0 ; sample < 48 ; sample++ )
+            {
+		    	while((Xmit & I2S2_IR) == 0);  // Wait for interrupt
+                I2S2_W0_MSW_W = (sinetable[sample]) ;
+                I2S2_W0_LSW_W = 0;
+                I2S2_W1_MSW_W = (sinetable[sample]) ;
+                I2S2_W1_LSW_W = 0;
+            }
+        }
+    }
+
 	// Режим «с микрофона на наушники»
 	aic3204_mic();
-
-	/* I2S settings */
-	I2S2_SRGR = 0x0015;
-	I2S2_ICMR = 0x0028;    // Enable interrupts
-	I2S2_CR = 0x8012;// 16-bit word, Master, enable I2C
 
 	for (i = 0; i < 5; i++) {
 		for (j = 0; j < 5000; j++) {
@@ -47,13 +77,13 @@ void main(void) {
 
 				/* Read Digital audio */
 				while ((Rcv & I2S2_IR) == 0); // Wait for receive interrupt to be pending
-				data3 = I2S2_W0_MSW_R; // 16 bit left channel received audio data
-				data4 = I2S2_W1_MSW_R;// 16 bit right channel received audio data
+				left = I2S2_W0_MSW_R; // 16 bit left channel received audio data
+				right = I2S2_W1_MSW_R;// 16 bit right channel received audio data
 
 				/* Write Digital audio */
 				while ((Xmit & I2S2_IR) == 0); // Wait for receive interrupt to be pending
-			I2S2_W0_MSW_W = data3;  // 16 bit left channel transmit audio data
-			I2S2_W1_MSW_W = data4;// 16 bit right channel transmit audio data
+				I2S2_W0_MSW_W = left;  // 16 bit left channel transmit audio data
+				I2S2_W1_MSW_W = right;// 16 bit right channel transmit audio data
 		}
 	}
 }
